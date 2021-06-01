8277b9b2b2e3dd264667f1bdf469a0b36df547d9
pavitra <83263125+pavitradalawai@users.noreply.github.com>
2021-06-01 16:18:13 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class BinarySearch{
+public static void main(String[]args){
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter number of records");
+int n=Sc.nextInt();
+int []arr=new int[n];
+System.out.println("Enter the "+ n +" values");
+for(int i=0;i<arr.length;i++){
+arr[i]=Sc.nextInt();
+}
+System.out.println("Enter the number that you have to Search");
+int number=Sc.nextInt();
+}
+}
+  
+
