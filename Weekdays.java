8619c621ec2cabd16787b8fb9436bb9158d720a4
pavitra <83263125+pavitradalawai@users.noreply.github.com>
2021-06-01 16:01:15 +0530

    Add files via upload

---
+++ Weekdays.java
@@ -0,0 +1,31 @@
+class Weekdays{
+public static void main(String[]args)
+{
+
+int day=2;
+switch(day){
+case 1:
+System.out.println("sunday");
+break;
+case 2:
+System.out.println("monday");
+break;
+case 3:
+System.out.println("Tuesday");
+break;
+case 4:
+System.out.println("Wednesday");
+break;
+case 5:
+System.out.println("Thursday");
+break;
+case 6:
+System.out.println("Friday");
+default:
+System.out.println("Saturday");
+break;
+}
+}
+}
+
+ 
