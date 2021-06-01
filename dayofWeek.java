0c49a199af008ad3f7a922b7abd697fa805207a7
pavitra <83263125+pavitradalawai@users.noreply.github.com>
2021-06-01 16:12:37 +0530

    Add files via upload

---
+++ dayofWeek.java
@@ -0,0 +1,28 @@
+public class DayOfWeek{
+public static void main(String[] args)
+{
+int dayOfWeek = 8;
+
+if(dayOfWeek == 1){
+System.out.println("Sunday");
+}
+else if(dayOfWeek == 2){
+System.out.println("Monday");
+}
+else if(dayOfWeek == 3){
+System.out.println("Tuesday");
+}
+else if(dayOfWeek == 4){
+System.out.println("Wednesday");
+}
+else if(dayOfWeek == 5){
+System.out.println("Thursday");
+}
+else if(dayOfWeek == 6){
+System.out.println("Friday");
+}
+else
+System.out.println("Saturday");
+}
+}
+             
---
+++ daysofweek.java
@@ -0,0 +1,27 @@
+public class dayOfWeek{
+public static void main(String[]args)
+{
+int daysOfWeek=8;
+if(daysOfWeek==1){
+System.out.println("Sunday");
+}
+else if(daysOfWeek==2){
+System.out.println("Monday");
+}
+else if(daysOfWeek==3){
+System.out.println("Tuesday");
+}
+else if(daysOfWeek==4){
+System.out.println("Wednesday");
+}
+else if(daysOfWeek==5){
+System.out.println("Thursday");
+}
+else if(daysOfWeek==6){
+System.out.println("Friday");
+}
+else
+System.out.println("Saturday");
+}
+}
+             
