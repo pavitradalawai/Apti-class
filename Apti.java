7b0c1876b85f7c710372f7fec74996a05738e3df
pavitra <83263125+pavitradalawai@users.noreply.github.com>
2021-08-04 16:33:28 +0530

    Add files via upload

---
+++ Apti.java
@@ -0,0 +1,19 @@
+import java .io.*;
+public class Apti
+public static printNumbers(int n)
+{
+int i,j;
+for(i=0; i<n; i++)
+{
+for(j=0;j<=i; j++){
+System.out.println(" ");
+}
+System.out.println();
+}
+}
+//driver function
+public stativ void main(String args[]){
+int n=5;
+printNumbers(n);
+}
+}
